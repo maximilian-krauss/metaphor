aabe5425448b26b7bb3de5c41a7ea90e3955cd04
Maximilian Krauss <max@krauss.io>
2015-08-05 22:50:23 +0200

    Added git commit id to heartbeat

--- controllers/heartbeat.js
+++ controllers/heartbeat.js
@@ -1,11 +1,16 @@
+var git = require('git-rev');
+
 module.exports = function() {
   var app = this.app,
       middlewares = this.middlewares;
 
   app.get('/heartbeat', function(req, res) {
-    res.status(200).send({
-      service: 'up and running',
-      mood: 'tipsy'
+    git.long(function(sha) {
+      res.status(200).send({
+        service: 'up and running',
+        commit: sha,
+        mood: 'tipsy'
+      });
     });
   });
 };
