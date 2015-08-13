35757a93a21ce1d086fbc4ddc669e1767cfa08ab
Maximilian Krauß <max@krauss.io>
2015-08-13 08:17:39 +0200

    Update custom-header.js

--- interceptors/custom-header.js
+++ interceptors/custom-header.js
@@ -3,6 +3,7 @@ module.exports = function() {
 
   app.use(function(req, res, next) {
     res.set('X-Krauss-Ping', 'Pong');
+    res.set('Arr-Disable-Session-Affinity', 'True');
     next();
   });
 }
